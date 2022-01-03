d1cca5c686f2af0ecc1ce6e8fbf12116a40b814b
Singappuli Arachchige Eranga Lakmal  Perera <45055405+eranga88@users.noreply.github.com>
2022-01-03 22:24:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req,res) => {
     console.log('New connection')
-    res.end('Hello Eranga')
+    res.end('Hello Eranga Lakmal Perera')
 })
 
 const PORT = process.env.PORT || 8080
